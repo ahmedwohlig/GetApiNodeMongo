afa3765f8638e3c788c931034508009eea1dcaa3
Ahmed <ahmedwohlig@gmail.com>
2021-09-15 16:43:55 +0530

    some changes

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ app.use(express.json());
 
 const port = process.env.PORT || 3002;
 
-require("./src/db/conn");
+mongoose.connect(
+    "Http://localhost:12707/MyDB",
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err) => {
+        if (err) {
+            console.log("MongoDB Connection Succeeded");
+        } else {
+            console.log("err in DB connection: " + err);
+        }
+    }
+);
 
 const Country = require("./src/models/Country");
 
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(
-    'Http://localhost:12707/MyDB',
-    { useNewUrlParser: true, useCreateIndex: true },
-    (err) => {
-        if (err) {
-            console.log('MongoDB Connection Succeeded');
-        } else {
-            console.log('err in DB connection: ' + err);
+module.exports = function () {
+    mongoose.connect(
+        "Http://localhost:12707/MyDB",
+        { useNewUrlParser: true, useCreateIndex: true },
+        (err) => {
+            if (err) {
+                console.log("MongoDB Connection Succeeded");
+            } else {
+                console.log("err in DB connection: " + err);
+            }
         }
-    }
-);
+    );
+};
